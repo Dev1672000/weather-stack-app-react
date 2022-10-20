e4a7ece0bf46323198cbdcf0df47afb235c9030f
Devendra Kumar Pandey <dev1672000@gmail.com>
2022-10-20 19:31:09 +0530

    upadate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const apiKey = "97454a422a013583b8eacb1a1ba1ceeb";
+  const [inputCity, setInputCity] = useState("");
+  const [data, setData] = useState({});
+
+  const getWetherDetails = (inputCity) => {
+    if (!inputCity) return;
+    const apiURL =
+      "https://openweathermap.org/api=" + inputCity + "&appid=" + apiKey;
+    axios
+      .get(apiURL)
+      .then((res) => {
+        console.log("response", res.data);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+
+  const handleChangeInput = (e) => {
+    console.log("value", e.target.value);
+    setInputCity(e.target.value);
+  };
+
+  const handleSearch = () => {
+    getWetherDetails(inputCity);
+  };
+  useEffect(() => {
+    getWetherDetails("delhi");
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="col-md-12">
+      <div className="wetherBg">
+        <h1 className="heading">Weather App</h1>
+
+        <div className="d-grid gap-3 col-4 mt-4">
+          <input
+            type="text"
+            className="form-control"
+            value={inputCity}
+            onChange={handleChangeInput}
+          />
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+
+      {Object.keys(data).length > 0 && (
+        <div className="col-md-12 text-center mt-5">
+          <div className="shadow rounded wetherResultBox">
+            <img
+              className="weathorIcon"
+              src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png"
+              alt="weatherIcon"
+            />
+
+            <h5 className="weathorCity">{data?.name}</h5>
+            <h6 className="weathorTemp">
+              {(data?.main?.temp - 273.15).toFixed(2)}°C
+            </h6>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
